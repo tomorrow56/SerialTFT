15ba9d76436ea1fc85c6bc562305368ff006ec59
ThousanDIY <tomorrow56@gmail.com>
2020-06-07 19:48:16 +0900

    update to screen refresh method

--- SerialTFT/examples/SPRESENSE_M5LCD_Factory_test/SPRESENSE_M5LCD_Factory_test.ino
+++ SerialTFT/examples/SPRESENSE_M5LCD_Factory_test/SPRESENSE_M5LCD_Factory_test.ino
@@ -17,6 +17,9 @@ float dht12_humidity;
 uint16_t AIN4;
 uint16_t AIN5;
 
+uint8_t X_pitch = 12;
+uint8_t Y_pitch = 18;
+
 uint16_t AIN4_MAX = 999; // ADC Value when 3.3V input
 uint16_t AIN5_MAX = 1007; // ADC Value when 3.3V input
 
@@ -32,7 +35,7 @@ void setup() {
   tft.begin(921600);
   tft.setRotation(1);
 
-  tft.setTextSize(3);
+  tft.setTextSize(2);
 
   tft.setCursor(0, 10);
   tft.setTextColor(BLUE, WHITE);
@@ -45,13 +48,29 @@ void setup() {
     Serial.println("BMP280 Device error!");
   }
 
+  tft.fillScreen(BLACK);
+
+  tft.setTextColor(GREEN);
+  tft.setCursor(0, 10);
+  tft.print("Temp_BMP280: ");
+  tft.setCursor(0, 10 + Y_pitch * 1);
+  tft.print("Temp_DHT12 : ");
+  tft.setCursor(0, 10 + Y_pitch * 2);
+  tft.print("Pressure   : ");
+  tft.setCursor(0, 10 + Y_pitch * 3);
+  tft.print("Humidity   : ");
+
+  tft.setTextColor(RED);
+  tft.setCursor(0, 10 + Y_pitch * 5);
+  tft.print("AIN4       : ");
+  tft.setCursor(0, 10 + Y_pitch * 6);
+  tft.print("AIN5       : ");
+
+  tft.setTextColor(WHITE, BLACK);
+
 }
 
 void loop() {
-
-  tft.fillScreen(BLACK);
-  tft.setCursor(0, 10);
-  tft.setTextColor(GREEN);
 
   pressure = bmp280.getPressure();
   temp = bmp280.getTemperature();
@@ -61,35 +80,36 @@ void loop() {
   Serial.print("Temp(BMP280): ");
   Serial.print(temp);
   Serial.println(" C"); // The unit for  Celsius because original arduino don't support speical symbols
-  tft.print("Temp_BMP280: ");
+  tft.setCursor(X_pitch * 13, 10);
+  tft.print("      C");
+  tft.setCursor(X_pitch * 13, 10);
   tft.print(String(temp));
-  tft.println(" C");
 
   Serial.print("Temp(DHT12) : ");
   Serial.print(dht12_temp);
   Serial.println(" C"); // The unit for  Celsius because original arduino don't support speical symbols
-  tft.print("Temp_DHT12 : ");
+  tft.setCursor(X_pitch * 13, 10 + Y_pitch * 1);
+  tft.println("      C");
+  tft.setCursor(X_pitch * 13, 10 + Y_pitch * 1);
   tft.print(String(dht12_temp));
-  tft.println(" C");
 
   Serial.print("Pressure: ");
   Serial.print(pressure / 100);
   Serial.println(" hPa");
-  tft.print("Pressure   : ");
+  tft.setCursor(X_pitch * 13, 10 + Y_pitch * 2);
+  tft.print("       hPa");
+  tft.setCursor(X_pitch * 13, 10 + Y_pitch * 2);
   tft.print(String(pressure / 100));
-  tft.println(" hPa");
 
   Serial.print("Humidity: ");
   Serial.print(dht12_humidity);
   Serial.println(" %RH");
-  tft.print("Humidity   : ");
+  tft.setCursor(X_pitch * 13, 10 + Y_pitch * 3);
+  tft.print("      %RH");
+  tft.setCursor(X_pitch * 13, 10 + Y_pitch * 3);
   tft.print(String(dht12_humidity));
-  tft.println(" %RH");
 
   Serial.println("\n");
-  tft.println();
-
-  tft.setTextColor(RED);
 
   AIN4 = analogRead(A2);
   AIN5 = analogRead(A3);
@@ -98,9 +118,15 @@ void loop() {
   
   Serial.println("AIN4=" + String(AIN4));
   Serial.println("AIN5=" + String(AIN5));
-  tft.println("AIN4= " + String(AIN4_V) + " V");
-  tft.println("AIN5= " + String(AIN5_V) + " V");
+  tft.setCursor(X_pitch * 13, 10 + Y_pitch * 5);
+  tft.print("     V");
+  tft.setCursor(X_pitch * 13, 10 + Y_pitch * 5);
+  tft.print(String(AIN4_V));
+  tft.setCursor(X_pitch * 13, 10 + Y_pitch * 6);
+  tft.print("     V");
+  tft.setCursor(X_pitch * 13, 10 + Y_pitch * 6);
+  tft.println(String(AIN5_V));
 
-  delay(1000);
+  delay(500);
 
 }
